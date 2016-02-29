8bad12ea4671341b55c22259e32178781f9d43f5
Marco Paga <marco.paga@codecentric.de>
2016-02-29 20:27:41 +0100

    first point extracted

--- test/analyze_runs/gpx_test.clj
+++ test/analyze_runs/gpx_test.clj
@@ -5,32 +5,29 @@
             [analyze-runs.gpx :refer :all]))
 
 (def points-from-sample-file (get-points "test/sample.gpx"))
+(def first-point (first points-from-sample-file))
 
 (deftest should-read-3-records-from-sample-file
   (is (= 3 (count points-from-sample-file))))
 
 (deftest assert-time-first-point
-  (let [first-point (first points-from-sample-file)]
-    (is (= (:time first-point) (tf/parse "2016-02-17T17:35:18Z")))))
+  (is (= (:time first-point) (tf/parse "2016-02-17T17:35:18Z"))))
 
 (deftest assert-elevation-first-point
-  (let [first-point (first points-from-sample-file)]
-    (is (= (:elevation first-point) 37.083679))))
+  (is (= (:elevation first-point) 37.083679)))
 
 (deftest assert-lat-first-point
-  (let [first-point (first points-from-sample-file)]
-    (is (= (:lat first-point) 50.671865))))
+  (is (= (:lat first-point) 50.671865)))
 
 (deftest assert-lon-first-point
-  (let [first-point (first points-from-sample-file)]
-    (is (= (:lon first-point) 7.187874))))
+  (is (= (:lon first-point) 7.187874)))
 
-(comment deftest assert-heart-rate-first-point
-  (let [first-point (first points-from-sample-file)]
-    (is (= (:heart-rate first-point) 114))))
+(deftest assert-heart-rate-first-point
+  (is (= (:heart-rate first-point) 114)))
 
 (deftest assert-elapsed-time
   (is (= 3 (calculate-time points-from-sample-file))))
 
 (deftest assert-distance
   (is (= 0.008250017987846405 (calculate-distance points-from-sample-file))))
+
